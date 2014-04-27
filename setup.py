4aa7dbfa9bbb81c54e831d084ce046f35beb8a17
Paymentwall Team <github@paymentwall.com>
2014-04-27 22:12:00 +0300

    MANIFEST.in to include non-py files in source distribution

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
 	name='paymentwall-python',
-	version='1.0.4',
+	version='1.0.5',
 	packages=['paymentwall'],
 	url='http://www.paymentwall.com',
 	description='Paymentwall Python Library',
