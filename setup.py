da66cdb2af8523cd4d60eb1b2b7fc3e9bfec3b0a
Ivan Kovalyov <ivan-kovalyov@users.noreply.github.com>
2015-08-20 13:40:19 +0300

    bump version id

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
 	name='paymentwall-python',
-	version='1.0.5',
+	version='1.0.6',
 	packages=['paymentwall'],
 	url='http://www.paymentwall.com',
 	description='Paymentwall Python Library',
