87bcea92306bf8986dacdd6dd1bbb1a483d4830f
Ivlev Denis <di-erz@yandex.ru>
2018-02-16 21:47:33 +0300

    Add __init__.py to src dir

---
+++ paymentwall/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from .base import Paymentwall
+from .pingback import Pingback
+from .product import Product
+from .widget import Widget
+
+__all__ = ['Paymentwall', 'Pingback', 'Product', 'Widget']
